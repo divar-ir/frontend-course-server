60dae3fdae68dd05b6c2f161c70408eb50e71ae0
Mohtada Hassanpour <mohtada.h@cafebazaar.ir>
2020-09-13 11:00:40 +0430

    :tada: initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const fastify = require('fastify')({
+    ignoreTrailingSlash: true,
+});
+const widgets = require('./widgets');
+const fullWidgets = require('./full-widgets');
+const prefix = 'api/v1';
+
+fastify.get(`${prefix}/widgets`, () => {
+    return widgets;
+});
+
+fastify.get(`${prefix}/widgets/actions`, () => {
+    return fullWidgets;
+});
+
+// Run the server!
+const start = async () => {
+    try {
+        await fastify.listen(process.env.PORT || 3000);
+        console.log(`server listening on ${fastify.server.address().port}`);
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+};
+
+start();
